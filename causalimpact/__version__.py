6228adbd3b6b8c196c7dbc587af8dfab447288c3
WillianFuks <willian.fuks@gmail.com>
2018-10-18 20:05:58 -0300

    Updated Version 0.0.5

--- causalimpact/__version__.py
+++ causalimpact/__version__.py
@@ -20,6 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-VERSION = (0, 0, 4)
+VERSION = (0, 0, 5)
 
 __version__ = '.'.join([str(e) for e in VERSION])
